dcf72bbd6bb59b331d8f5c14caefba3a4bf8799c
dk2208 <63074012+dk2208@users.noreply.github.com>
2020-04-03 17:00:54 +0530

    Create question.js

---
+++ question.js
@@ -0,0 +1,142 @@
+var questions=[{
+  "question":"Tags and test that are not directly displayes on the page are written in which section?",
+  "option1":"<title>",
+  "option2":"<head>",
+  "option3":"<html>",
+  "option4":"<body>",
+  "answer":"2"
+},{
+  "question":"How can you make a numbered list and a bulleted list?",
+  "option1":"<list>,<ul>",
+  "option2":"<ol>,<ol>",
+  "option3":"<ul>,<ol>",
+  "option4":"<ol>,<ul>",
+  "answer":"4"
+},{
+  "question":"In CSS,what is the correct option to select all the tags on a page?",
+  "option1":".p{}",
+  "option2":"<p>{}",
+  "option3":"p{}",
+  "option4":"#p{}",
+  "answer":"3"
+},{
+  "question":"In CSS,Select the appropriate option to style an element so that the next element would appear right next to it not underneath it if both elements widths were collectively smaller than the container element?",
+  "option1":"display:inline",
+  "option2":"display:horizontal",
+  "option3":"display:left",
+  "option4":"None",
+  "answer":"1"
+
+},{
+  "question":"Select the property used to create space between the element's border and inner content?",
+  "option1":"padding",
+  "option2":"spacing",
+  "option3":"border",
+  "option4":"margin",
+  "answer":"1"
+},{
+  "question":"Which of the following statements is valid for the features of JavaScipt?",
+  "option1":"JavaScipt is complementary to and integrated with Java.",
+  "option2":"JavaScript is designed for creating network-centric applications.",
+  "option3":"JavaScript is a lightweight,interpreted programming language.",
+  "option4":"All",
+  "answer":"4"
+},{
+  "question":"Which of the following method of Boolean object returns a string depending upon the value of the object?",
+  "option1":"valueOf()",
+  "option2":"toSource()",
+  "option3":"toString()",
+  "option4":"None",
+  "answer":"3"
+},{
+   "question":"What is the function of Array object that runs through each element of the array?",
+   "option1":"concat()",
+   "option2":"every()",
+   "option3":"forEach()",
+   "option4":"filter()",
+   "answer":"3"
+ },{
+   "question":"Does setting margin-top and margin-bottom have an effect on an inline element?",
+   "option1":"Yes",
+   "option2":"No",
+   "option3":"Sometimes",
+   "option4":"None",
+   "answer":"2"
+ },{
+   "question":"Which among the following is a correct way to open a Hyperlink in new tab?",
+   "option1":'<a href="https://www.progate.com"newtab></a>',
+   "option2":'<a href="https://www.progate.com" target=newtab></a>',
+   "option3":'<a href="https://www.progate.com" target="_blank"></a>',
+   "option4":'<a href="https://www.progate.com"></a>',
+   "answer":"3"
+ },{
+   "question":"Which of the following is a true statement for JavaScript callbacks?",
+   "option1":"A callback is a plain JavaScript function passed to some method as an argument or option.",
+   "option2":"Some callbacks are just events,called to give the user a chance to react when a certain state is triggered.",
+   "option3":"None",
+   "option4":"All except None.",
+   "answer":"4"
+ },{
+   "question":"Which of the following elements can be used in HTML to create a table?",
+   "option1":"<table>,<tbody>,<trow>",
+   "option2":"<table>,<tb>,<trow>",
+   "option3":"<table>,<tbody>,<tr>",
+   "option4":"All",
+   "answer":"3"
+ },{
+   "question":'Which of the following is the correct syntax to display "Hello World" in an alert box using JavaScript?',
+   "option1":'alertbox("Hello World");',
+   "option2":'msg("Hello World");',
+    "option3":'msgbox("Hello World");',
+    "option4":'alert("Hello World");',
+    "answer":"4"
+  },{
+    "question":'Predict the output of the following=>  console.log(4+3+2+"1"); ?',
+    "option1":"4321",
+    "option2":"10",
+    "option3":"91",
+    "option4":"433",
+    "answer":"3"
+  },{
+    "question":"Which is the correct syntax to set the unordered-list item marker to a square?",
+    "option1":'<ul style="list-style-type:square">',
+    "option2":'<ul style="square">',
+    "option3":'<ul list-style:"square">',
+    "option4":"<ul style=square>",
+    "answer":"1"
+  },{
+    "question":'Predict the output of the following=>  console.log("4"+3+2+1); ?',
+    "option1":"4321",
+    "option2":"10",
+    "option3":"91",
+    "option4":"46",
+    "answer":"1"
+  },{
+    "question":"If you have a page of search results and want to highlight the search term,what HTML tag would you use?",
+    "option1":"<strong>",
+    "option2":"<mark>",
+    "option3":"<em>",
+    "option4":"<highlight>",
+    "answer":"2"
+  },{
+    "question":"Which of the following property of a anchor element signifies an element on which the user is currently clicking?",
+    "option1":":link",
+    "option2":":visited",
+    "option3":":hover",
+    "option4":":active",
+    "answer":"4"
+  },{
+    "question":'Choose the correct JavaScript syntax to change the content of the following HTML code =>   <p id="Progate">Learn to code,learn to be creative.</p>',
+    "option1":'document.getElement("Progate").innerHTML="Learn to code,learn to be creative.";',
+    "option2":'document.getElementById("Progate").innerHTML="Learn to code,learn to be creative.";',
+    "option3":'document.getId("Progate")="Learn to code,learn to be creative.";',
+    "option4":'document.getElementById("Progate").innerHTML=Learn to code,learn to be creative.;',
+    "answer":"2"
+  },{
+    "question":"Which of the following function of String object produces an HTML hypertext link for requesting another URL?",
+    "option1":"small()",
+    "option2":"link()",
+    "option3":"sub()",
+    "option4":"sup()",
+    "answer":"2"
+  }]
